8cc99f6f6b09dbc31de7360ed2474a8a9a4a175f
shiganai09 <shiganai09@gmail.com>
2018-07-27 17:48:43 +0900

    offline OK

--- opencv_wall/main.cpp
+++ opencv_wall/main.cpp
@@ -8,20 +8,24 @@ int main()
 	//cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);//解像度 横(予備)
 	//cap.set(CV_CAP_PROP_FRAME_HEIGHT, 960);//解像度 縦(予備)
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
 	cap.set(CV_CAP_PROP_EXPOSURE, -13);//露出を下げてシャッター速度を上げる
 
+	/*
 	if (!cap.isOpened())//カメラデバイスが正常にオープンしたか確認
 	{
 		//読み込みに失敗したときの処理
 		return -1;
 	}
+	*/
+	cv::Mat pict = cv::imread("testPict.png");
 
 	while (1) {
 		cv::Mat color;//カラー画像
 		cv::Mat gray;//グレースケール画像
 
-		cap >> color;//USBカメラからの画像を入力
+		//pict >> color;//USBカメラからの画像を入力
+		color = pict.clone();
 
 		cv::cvtColor(color, gray, cv::COLOR_BGR2GRAY);//グレースケール化
 		cv::Mat bin;//二値化画像
