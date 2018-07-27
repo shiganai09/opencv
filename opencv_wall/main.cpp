ede285bd7002e6ed0b7dfcbb1f03483d2671bc90
shiganai09 <shiganai09@gmail.com>
2018-07-27 16:01:27 +0900

    web cam test

--- opencv_wall/main.cpp
+++ opencv_wall/main.cpp
@@ -1,10 +1,32 @@
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
-
 int main()
 {
-	Mat image = Mat::zeros(100, 100, CV_8UC3);
-	imshow("", image);
-	waitKey(0);
+	/*Webカメラ初期設定*/
+	cv::VideoCapture cap(0);//デバイスのオープン(エラーが出る使う場合'1'かもしれない)
+	cap.set(cv::CAP_PROP_FPS, 30.0);
+	//cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);//解像度 横(予備)
+	//cap.set(CV_CAP_PROP_FRAME_HEIGHT, 960);//解像度 縦(予備)
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+	cap.set(CV_CAP_PROP_EXPOSURE, -13);//露出を下げてシャッター速度を上げる
+
+	if (!cap.isOpened())//カメラデバイスが正常にオープンしたか確認
+	{
+		//読み込みに失敗したときの処理
+		return -1;
+	}
+
+	while (1) {
+		cv::Mat color;//カラー画像
+
+		cap >> color;//USBカメラからの画像を入力
+
+
+		cv::imshow("window", color);//画像を表示．
+
+		int key = cv::waitKey(10);
+
+	}
 }
+
