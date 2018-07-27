9dc9bd57dd19b5eb3befd219d14342dd964c3f67
shiganai09 <shiganai09@gmail.com>
2018-07-27 17:30:58 +0900

    binary effect

--- opencv_wall/main.cpp
+++ opencv_wall/main.cpp
@@ -7,7 +7,7 @@ int main()
 	cap.set(cv::CAP_PROP_FPS, 30.0);
 	//cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);//解像度 横(予備)
 	//cap.set(CV_CAP_PROP_FRAME_HEIGHT, 960);//解像度 縦(予備)
-	cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
 	cap.set(CV_CAP_PROP_EXPOSURE, -13);//露出を下げてシャッター速度を上げる
 
@@ -19,11 +19,17 @@ int main()
 
 	while (1) {
 		cv::Mat color;//カラー画像
+		cv::Mat gray;//グレースケール画像
 
 		cap >> color;//USBカメラからの画像を入力
 
+		cv::cvtColor(color, gray, cv::COLOR_BGR2GRAY);//グレースケール化
+		cv::Mat bin;//二値化画像
+		cv::threshold(gray, bin, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
 
-		cv::imshow("window", color);//画像を表示．
+		
+
+		cv::imshow("window", bin);//画像を表示．
 
 		int key = cv::waitKey(10);
 
